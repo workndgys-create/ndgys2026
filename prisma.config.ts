f0fd76edb1bcbd478b5f46f9f01550b6d0c5d71b
Ritvik Rana <105637455+riitvikk@users.noreply.github.com>
2026-06-08 01:55:11 +0530

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,6 @@
-
 import "dotenv/config";
-
 import { defineConfig } from "prisma/config";
-
 const databaseUrl = process.env.DATABASE_URL || "file:./dev.db";
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
