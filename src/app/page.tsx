c74f6f3404ea900b279e36eaeebf30749a932de1
Ritvik Rana <105637455+riitvikk@users.noreply.github.com>
2026-06-07 22:40:19 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,9 @@ export const dynamic = "force-dynamic";
 export default async function HomePage() {
   return (
     <>
-      <Marquee />
+      
       <Navbar />
+      <Marquee />
       <main>
         <Hero />
         <About />
