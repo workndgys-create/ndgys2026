ec8de4f3fb238b2169cdd43ed499ba04b1c57251
Ritvik Rana <105637455+riitvikk@users.noreply.github.com>
2026-06-09 17:26:31 +0530

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,10 +2,6 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client";
-
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
