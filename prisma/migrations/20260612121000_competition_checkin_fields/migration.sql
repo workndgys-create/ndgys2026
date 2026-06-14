9453fdeb86e1240afaaf277ce554ed908d5342ff
Ritvik Rana <105637455+riitvikk@users.noreply.github.com>
2026-06-14 08:12:48 +0530

    Create migration.sql

---
+++ prisma/migrations/20260612121000_competition_checkin_fields/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "CompetitionRegistration" ADD COLUMN "checkedInDay1" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "CompetitionRegistration" ADD COLUMN "checkedInDay2" BOOLEAN NOT NULL DEFAULT false;
